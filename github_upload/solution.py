9af2604d88520578e589321066560827a1f158a7
Toshi <huynhduykhoi619@gmail.com>
2022-07-12 10:44:36 +0700

    Numpy basic exercise 3

--- github_upload/solution.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-if __name__ == "__main__":
-    arr1 = np.array([0,0,0,0], dtype=int)
-    arr2 = np.array([0,0,0,1], dtype=int)
-    print(np.any(arr1))
-    print(np.any(arr2))
