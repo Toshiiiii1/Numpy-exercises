c09a3ab478a5c574f1bd5bfe7b5ba978d64e4870
Toshi <huynhduykhoi619@gmail.com>
2022-07-12 10:41:40 +0700

    Numpy basic exercise 4

--- github_upload/solution.py
+++ github_upload/solution.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 if __name__ == "__main__":
-    arr = np.array((np.arange(1,7), np.arange(1,7)), dtype=int)
-    if (0 in arr):
-        print("yes")
-    else:
-        print("no")
+    arr1 = np.array([0,0,0,0], dtype=int)
+    arr2 = np.array([0,0,0,1], dtype=int)
+    print(np.any(arr1))
+    print(np.any(arr2))
